2b9648e883c0072d9b99730dc555f7bf5e9f0140
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-06-25 22:32:22 -0300

    Add files via upload

---
+++ menu-float-html/script.js
@@ -0,0 +1,8 @@
+const carrinho = document.getElementById("Carrinho")
+
+function CarrinhoOn(){
+    carrinho.style = 'display: block; position: fixed;';
+}
+function CarrinhoOff(){
+    carrinho.style = 'display: none';
+}
